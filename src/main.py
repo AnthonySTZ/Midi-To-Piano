bc03c24f56645d13e04a28205cb4f9d5e6e0cc8f
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-18 12:27:46 +0100

    show messages from track

--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@ import midi_handler as midi
 
 if __name__ == "__main__":
     midi_file: midi.mido.MidiFile = midi.read_from_file("assets\YLIA.mid")
-    midi.show_tracks_infos(midi_file)
+    midi.show_messages_from_track(midi_file.tracks[2], 17, 10)
