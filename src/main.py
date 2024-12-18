2c50ea821350c51159e07bb747a4b71be5b79e84
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-18 11:55:56 +0100

    read midi_file to mido.MidiFile

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-
+import midi_handler as midi
 
 if __name__ == "__main__":
-    pass
+    midi_file: midi.mido.MidiFile = midi.read_from_file("assets\YLIA.mid")
+    print(midi_file)
--- src/midi_handler.py
+++ src/midi_handler.py
@@ -1,2 +1,4 @@
 import mido
 
+def read_from_file(filename:str)->mido.MidiFile:
+    return mido.MidiFile(filename)
