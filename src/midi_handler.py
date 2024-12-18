dc550eba96937ba65cc336c2d9f979a806b32b2d
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-18 12:11:35 +0100

    type hint

--- src/midi_handler.py
+++ src/midi_handler.py
@@ -5,8 +5,9 @@ def read_from_file(filename:str)->mido.MidiFile:
 
 def show_tracks_infos(midi_file:mido.MidiFile) -> None:
     for i, track in enumerate(midi_file.tracks):
+        track: mido.MidiTrack
         track_meta_messages = get_meta_messages_from_track(track)
         print(f"Track {i}: {track.name if track.name else 'No Name'} with {len(track)} messages including {len(track_meta_messages)} meta messages")
 
-def get_meta_messages_from_track(track: mido.midifiles.tracks.MidiTrack):
+def get_meta_messages_from_track(track: mido.MidiTrack):
     return list(filter(lambda msg: msg.is_meta, track))
