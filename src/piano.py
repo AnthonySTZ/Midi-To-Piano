76e0359c114d545b3e54b780cfb4cf537b74e99d
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-18 20:31:02 +0100

    append all notes to track messages

--- src/piano.py
+++ src/piano.py
@@ -7,7 +7,7 @@ def run():
     for track in midi_file.tracks:
         messages: list[dict] = midi.get_all_notes_from_track(track)
         if messages:
-            tracks_messages.append(messages[5])
+            tracks_messages.append(messages)
 
     context = hou_nodes.get_curr_context()
     hou_nodes.create_channel_node(context, 88)
