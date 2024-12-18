65d6304e0debbfcc6f218a6c635e37ee2b7bab2e
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-18 12:09:25 +0100

    get tracks infos

--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@ import midi_handler as midi
 
 if __name__ == "__main__":
     midi_file: midi.mido.MidiFile = midi.read_from_file("assets\YLIA.mid")
-    print(midi_file)
+    midi.show_tracks_infos(midi_file)
--- src/midi_handler.py
+++ src/midi_handler.py
@@ -2,3 +2,11 @@ import mido
 
 def read_from_file(filename:str)->mido.MidiFile:
     return mido.MidiFile(filename)
+
+def show_tracks_infos(midi_file:mido.MidiFile) -> None:
+    for i, track in enumerate(midi_file.tracks):
+        track_meta_messages = get_meta_messages_from_track(track)
+        print(f"Track {i}: {track.name if track.name else 'No Name'} with {len(track)} messages including {len(track_meta_messages)} meta messages")
+
+def get_meta_messages_from_track(track: mido.midifiles.tracks.MidiTrack):
+    return list(filter(lambda msg: msg.is_meta, track))
