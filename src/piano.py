3876d3fd44ce38792384f8c7590ee61de69421bc
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-19 11:23:50 +0100

    key every tracks

--- src/piano.py
+++ src/piano.py
@@ -3,14 +3,14 @@ import hou_nodes
 
 
 def run():
-    midi_file: midi.mido.MidiFile = midi.read_from_file(
-        "C:\\Disque E\\Prog\\python_midi\\assets\\YLIA.mid"
-    )
+    midi_path = "C:\\Disque E\\Prog\\python_midi\\assets\\YLIA.mid"
+    midi_file: midi.mido.MidiFile = midi.read_from_file(midi_path)
     tracks = midi.get_all_tracks_from_midi_file(midi_file)
 
     context = hou_nodes.get_curr_context()
     channel_node = hou_nodes.create_channel_node(context, 88)
-    hou_nodes.animate_notes_by_track(channel_node, tracks[0], 1010)
+    for track in tracks:
+        hou_nodes.animate_notes_by_track(channel_node, track, 1010)
     hou_nodes.transfer_chop_to_attrib(hou_nodes.get_selected_node(), channel_node)
 
 
