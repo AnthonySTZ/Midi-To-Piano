ed0cdbadde7a1bf3253899228ea07b25517e021d
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-19 12:05:35 +0100

    set interface UI

--- src/interface.py
+++ src/interface.py
@@ -1,5 +1,5 @@
 import hou  # type: ignore
-from PySide2.QtWidgets import QDialog, QWidget, QVBoxLayout, QPushButton
+from PySide2.QtWidgets import QDialog, QVBoxLayout, QPushButton, QLineEdit, QSizePolicy
 from PySide2.QtCore import Qt
 
 
@@ -18,14 +18,34 @@ class MainWindow(QDialog):
     def set_window_settings(self) -> None:
         self.setParent(hou.ui.mainQtWindow(), Qt.Window)
         self.setWindowTitle("Midi Piano")
-        self.resize(300, 200)
+        self.resize(300, 150)
 
     def set_ui(self) -> None:
+        self.setStyleSheet(
+            """
+            font-size: 11pt;
+            """
+        )
         central_layout = QVBoxLayout()
         self.setLayout(central_layout)
 
+        self.file_path_edit = QLineEdit()
+        self.file_path_edit.setPlaceholderText("Enter MIDI file path...")
+        self.file_path_edit.setStyleSheet(
+            """
+            QLineEdit{
+                padding: 2px;
+            }
+            """
+        )
+        central_layout.addWidget(self.file_path_edit)
+
         self.browse_file = QPushButton("Browse Midi file")
+        self.browse_file.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         central_layout.addWidget(self.browse_file)
+
+        self.accept_btn = QPushButton("Accept")
+        central_layout.addWidget(self.accept_btn)
 
 
 def create_window():
